a6e1b488b5931861fdac3b7bc7a36a41698e4d0d
Damilola Sigbeku <Dsigbeku@yahoo.com>
2020-07-31 02:36:25 -0500

    finished the orbit list

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -13,4 +13,13 @@ export class Satellite {
         this.launchDate = launchDate;
 
     }
+    shouldShowWarning(): boolean{
+
+        if(this.type.toLowerCase() === 'space debris') {
+            return true;
+        }
+    else {
+        return false;
+    }
+    }
 }
